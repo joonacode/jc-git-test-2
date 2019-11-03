2e50c1755ed3fe5473e8dc8d1aff22b593827d3b
joonacode <joonacode@gmail.com>
2019-11-02 21:46:09 -0700

    Menambahkan list

--- indek.php
+++ indek.php
@@ -8,6 +8,10 @@
 </head>
 <body>
     <h1>Hallo World</h1>
+    <ul>
+        <li>asd</li>
+        <li>asdadas</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae id explicabo sit mollitia quos, culpa esse vero voluptates quidem labore at quo sint. Doloribus recusandae neque debitis sunt amet nihil!</p>
 </body>
 </html>
